3df97cb10756f063a9f2e475dd2cdcdc78efbd55
Krzysztof Zaporowski <krzysztofzaporowski@gmail.com>
2026-03-26 18:57:48 +0100

    add sinogram filter and invesrse Radon transform

--- tomograf/utils.py
+++ tomograf/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pydicom.dataset import Dataset, FileDataset 
 
 def wyznacz_pozycje_czujnikow(stopnie_alfa, n, rozpietosc_stopnie, promien_r, srodek_x, srodek_y):
     alfa = np.radians(stopnie_alfa)
@@ -67,3 +68,32 @@ def bresenham(x0, y0, x1, y1):
             y0 += sy
 
     return points
+
+def filtruj_sinogram(sinogram):
+    liczba_detektorow, liczba_skanow = sinogram.shape
+    przefiltrowany_sinogram = np.zeros_like(sinogram)
+
+    czestotliwosci = np.fft.fftfreq(liczba_detektorow)    
+    filtr_ramp = np.abs(czestotliwosci) * 2.0 
+
+    for skan in range(liczba_skanow):
+        odczyt_kat = sinogram[:, skan]
+        odczyt_fft = np.fft.fft(odczyt_kat)
+        odczyt_przefiltrowany_fft = odczyt_fft * filtr_ramp
+
+        odczyt_przefiltrowany = np.real(np.fft.ifft(odczyt_przefiltrowany_fft))
+
+        przefiltrowany_sinogram[:, skan] = odczyt_przefiltrowany
+
+    return przefiltrowany_sinogram
+
+def policz_mse(obraz_oryginalny, obraz_rekonstrukcji):
+    ori_norm = obraz_oryginalny / np.max(obraz_oryginalny)
+
+    if np.max(obraz_rekonstrukcji) > 0:
+        rek_norm = obraz_rekonstrukcji / np.max(obraz_rekonstrukcji)
+    else:
+        rek_norm = obraz_rekonstrukcji
+
+    mse = np.mean((ori_norm - rek_norm) ** 2)
+    return mse
